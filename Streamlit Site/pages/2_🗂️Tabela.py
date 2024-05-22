b43a787db419a411150980ddb64e2ee747e10c10
Henrique Oliveira <61119602+henrioliveiragit@users.noreply.github.com>
2024-05-22 16:13:16 -0300

    Delete Streamlit Site directory

--- Streamlit Site/pages/2_🗂️Tabela.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.write("# 🗂️Tabela Importada do Excel")
-st.text("🐼 Usando Pandas 2.2.2")
-df = pd.read_csv("Contoso - Lojas.csv", sep=';', encoding='utf-8-sig')
-
-st.table(data=df)
